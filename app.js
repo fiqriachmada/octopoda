917ba9f9d691fae62aa8de5a658d3b681e81ebd1
fiqriachmada <achmada1998@gmail.com>
2023-05-15 18:15:30 +0700

    new

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const postUsers = require('./app/api/v1/users/postUsers');
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+// const getUsers = require('./app/api/v1/users/getUsers');
+
+const app = express();
+const port = 50001;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json(['Welcome to Octopoda version: 1 ']);
+});
+
+// app.use('/users', getUsers);
+
+// app.use('/users', postUsers);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
